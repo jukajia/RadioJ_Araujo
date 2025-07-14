7c5577225663b521e643ff5f75986f00a41d3126
Juka Produções <82520164+jukajia@users.noreply.github.com>
2025-07-14 03:00:19 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,29 @@
+const CACHE_NAME = 'jotta-araujo-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+];
 
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
